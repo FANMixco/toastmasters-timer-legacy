92f700ea90799fd9a6b52549750930b22534b187
Federico Navarrete <darklord.navarrete@gmail.com>
2018-01-24 15:21:16 +0100

    Update tmTimer.js

--- js/tmTimer.js
+++ js/tmTimer.js
@@ -242,11 +242,11 @@ $(function(){
         }).val("").trigger("change");
 	});
 	
-	$("#inlineTimer").colorbox({inline:true, width:"80%", onComplete: function(){
+	$("#inlineTimer").colorbox({inline:true, width:"90%", onComplete: function(){
         $('#cboxContent').css('background-color', lastColor);
     }});
 	
-	$("#inlineTimetable").colorbox({inline:true, width:"80%", onComplete: function(){
+	$("#inlineTimetable").colorbox({inline:true, width:"90%", onComplete: function(){
         $('#cboxContent').css('background', "white");
     }});
 	
