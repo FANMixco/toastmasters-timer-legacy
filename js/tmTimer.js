0529c578c57f56f50b43c3c4edca87732e86c8f6
Federico Navarrete <darklord.navarrete@gmail.com>
2018-02-12 07:52:54 +0100

    Add files via upload
    Added validation for iOS and Android devices.

--- js/tmTimer.js
+++ js/tmTimer.js
@@ -45,6 +45,32 @@ function getTime(){
 	return (new Date).clearTime().addSeconds(counter).toString('H:mm:ss');
 }
 
+/**
+ * Determine the mobile operating system.
+ * This function returns one of 'iOS', 'Android', 'Windows Phone', or 'unknown'.
+ *
+ * @returns {String}
+ */
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+      // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+
+    return "unknown";
+}
+
 function btnStopClick(isAdded) {
 	var member = $("#txtMember").val();
 	var currentTime = getTime();
@@ -378,4 +404,9 @@ $(function(){
 	getBeep();
 	setImgAndBng();
 	setVolumeImg();
+	
+	var os = getMobileOperatingSystem();
+	if (os == "iOS" || os == "Android"){
+		$("#btnBeep").hide();
+	}
 });
